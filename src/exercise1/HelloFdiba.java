bdbca411b6c010dfc4f8ec897d7004876985434c
vikisimidova02 <>
2021-10-13 14:58:28 +0000

    Files updated

--- src/exercise1/HelloFdiba.java
+++ src/exercise1/HelloFdiba.java
@@ -3,7 +3,7 @@ package exercise1;
 public class HelloFdiba {
 
     public static void main(String[] args) {
-        System.out.println("Hello Fdiba!");
+        System.out.println("Hello FDIBA!");
     }
 
 }
--- src/exercise1/Tree.java
+++ src/exercise1/Tree.java
@@ -3,12 +3,12 @@ package exercise1;
 public class Tree {
 
     public static void main(String[] args) {
-        System.out.println("    *     ");
-        System.out.println("   ***    ");
-        System.out.println("  *****   ");
-        System.out.println(" *******  ");
-        System.out.println("********* ");
-        System.out.println("   ***    ");
+        System.out.println("    *");
+        System.out.println("   ***");
+        System.out.println("  *****");
+        System.out.println(" *******");
+        System.out.println("*********");
+        System.out.println("   ***");
     }
 
 }
